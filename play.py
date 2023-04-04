d42ea7e1c124e33aef6c40bbbc2b1b7d644009f3
AI-Gio <60139940+AI-Gio@users.noreply.github.com>
2023-04-04 12:07:33 +0000

    deleted line 18 from common.py

--- play.py
+++ play.py
@@ -5,6 +5,23 @@ IMAGE_TRAIN_DIR = "/workspaces/SuperYOLOH/dataset/VEDAI"
 
 
 
-for directory in [IMAGE_TRAIN_DIR, IMAGE_VAL_DIR]:
-    with open(os.path.join(directory, "image_files.txt"), mode="w") as f:
-        f.writelines([os.path.join(directory, "images", image_file) for image_file in os.listdir(directory, "images")])
+# remove all of the infrared images from VEDAI/images
+for f_name in os.listdir("/workspaces/SuperYOLOH/dataset/VEDAI/images"):
+    if "_ir" in f_name:
+        os.remove(f"/workspaces/SuperYOLOH/dataset/VEDAI/images/{f_name}")
+
+# remove "_co" from
+# loop through all files in the directory
+for filename in os.listdir(IMAGE_TRAIN_DIR + "/images"):
+    
+    # check if the filename contains '_co'
+    if '_co' in filename:
+        
+        # construct the new filename by removing '_co'
+        new_filename = filename.replace('_co', '')
+        
+        # rename the file
+        os.rename(os.path.join(IMAGE_TRAIN_DIR + "/images", filename), os.path.join(IMAGE_TRAIN_DIR + "/images", new_filename))
+
+
+python train.py --cfg models/SRyolo_noFocus_small.yaml --super --train_img_size 1024 --hr_input --data data/SRvedai.yaml --ch 3 --input_mode RGB
